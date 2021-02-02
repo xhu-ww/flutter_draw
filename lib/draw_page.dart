3f266f17f18cf2a6557291cd53ebd8a605453c77
王文 <xhuww2012@gmail.com>
2021-02-02 18:41:35 +0800

    add features

--- lib/draw_page.dart
+++ lib/draw_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_draw/draw_painter.dart';
@@ -8,29 +10,128 @@ class DrawPage extends StatefulWidget {
 }
 
 class _DrawPageState extends State<DrawPage> {
-  List<Offset> offsets = [];
+  var _path = Path();
+  var _color = Colors.red;
+  var _lineWidth = 5.0;
+  var _onEraseMode = false;
+  Offset _currentOffset;
+  final List<Line> _undoLines = [];
+  final List<Line> _lines = [];
+
+  void _goBack() {
+    if (_lines.isNotEmpty) {
+      setState(() {
+        _undoLines.add(_lines.removeLast());
+      });
+    }
+  }
+
+  void _reset() {
+    setState(() {
+      _lines.clear();
+      _undoLines.clear();
+    });
+  }
+
+  void _forward() {
+    if (_undoLines.isNotEmpty) {
+      var last = _undoLines.last;
+      _lines.add(last);
+      _undoLines.removeLast();
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            mini: true,
+            child: Icon(
+              Icons.style,
+              color: _onEraseMode ? Colors.grey : Colors.white,
+            ),
+            onPressed: () {
+              setState(() => _onEraseMode = !_onEraseMode);
+            },
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            mini: true,
+            child: Icon(Icons.arrow_back),
+            onPressed: _goBack,
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            mini: true,
+            child: Icon(Icons.replay),
+            onPressed: _reset,
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            mini: true,
+            child: Icon(Icons.arrow_forward_rounded),
+            onPressed: _forward,
+          ),
+        ],
+      ),
       body: GestureDetector(
         onPanStart: (details) {
           setState(() {
-            offsets.add(details.globalPosition);
+            _undoLines.clear();
+            _currentOffset = details.globalPosition;
+            _path.moveTo(_currentOffset.dx, _currentOffset.dy);
           });
         },
         onPanUpdate: (details) {
           setState(() {
-            offsets.add(details.globalPosition);
+            _currentOffset = details.globalPosition;
+            _path.lineTo(_currentOffset.dx, _currentOffset.dy);
           });
         },
         onPanEnd: (details) {
+          setState(() {
+            _currentOffset = null;
+            _lines.add(
+              Line(
+                path: Path.from(_path),
+                color: _color,
+                width: _lineWidth,
+                eraseMode: _onEraseMode,
+              ),
+            );
+            _path.reset();
+          });
         },
         child: CustomPaint(
           size: Size.infinite,
-          painter: DrawPainter(offsets),
+          painter: DrawPainter(
+            _lines,
+            Line(
+              path: Path.from(_path),
+              color: _color,
+              width: _lineWidth,
+              eraseMode: _onEraseMode,
+            ),
+            currentOffset: _currentOffset,
+          ),
         ),
       ),
     );
   }
+}
+
+class Line {
+  Path path;
+  Color color;
+  double width;
+  bool eraseMode;
+
+  Line({this.path, this.color, this.width, this.eraseMode = false})
+      : assert(path != null),
+        assert(eraseMode != null);
 }
--- lib/draw_painter.dart
+++ lib/draw_painter.dart
@@ -1,18 +1,48 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class DrawPainter extends CustomPainter {
-  final List<Offset> points;
+import 'draw_page.dart';
 
-  DrawPainter(this.points);
+const ERASER_SIZE = 20.0;
+
+class DrawPainter extends CustomPainter {
+  final List<Line> lines;
+  final Line currentLine;
+  final Offset currentOffset;
+
+  DrawPainter(this.lines, this.currentLine, {this.currentOffset})
+      : assert(lines != null),
+        assert(currentLine != null);
 
   @override
   void paint(Canvas canvas, Size size) {
-    if (points == null) return;
-    Paint paint = Paint()..color = Colors.red;
-    for (Offset point in points) {
-      canvas.drawCircle(point, 2, paint);
+    canvas.saveLayer(Offset.zero & size, Paint());
+    for (Line line in lines) {
+      _drawLine(canvas, line);
     }
+    _drawLine(canvas, currentLine);
+    canvas.restore();
+    if (currentLine.eraseMode) {
+      canvas.drawRect(
+          Rect.fromCenter(
+            center: currentOffset,
+            width: ERASER_SIZE,
+            height: ERASER_SIZE,
+          ),
+          Paint()..color = Colors.grey);
+    }
+  }
+
+  void _drawLine(Canvas canvas, Line line) {
+    canvas.drawPath(
+      line.path,
+      Paint()
+        ..color = line.color ?? Colors.blue
+        ..strokeWidth = line.eraseMode ? ERASER_SIZE : line.width ?? 5.0
+        ..style = PaintingStyle.stroke
+        ..blendMode = line.eraseMode ? BlendMode.clear : BlendMode.srcOver,
+    );
   }
 
   @override
